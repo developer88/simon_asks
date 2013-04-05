837d78476e704b411602f0d496b66536a8a1985f
Eremin Andrey <dsoft88@gmail.com>
2013-04-05 23:25:26 +0400

    [Changed] engine class.

--- lib/simon_asks/engine.rb
+++ lib/simon_asks/engine.rb
@@ -1,5 +1,17 @@
 module SimonAsks
   class Engine < ::Rails::Engine
     isolate_namespace SimonAsks
+
+    # Precompile any assets included straight in certain pges
+    initializer "simon_asks.assets.precompile", :group => :all do |app|
+     # app.config.assets.precompile += %w[
+      #  forem/admin/members.js
+      #]
+    end
+
+
   end
 end
+
+require 'acts_as_votable'
+require 'auto_html_for'
