dc397811d1aa859840212e510470eb1a9ad44bbc
Eremin Andrey <dsoft88@gmail.com>
2013-04-08 14:24:31 +0400

    [Finished] with Question Resource for ActiveAdmin.

--- lib/generators/simon_asks/install/templates/questions.rb
+++ lib/generators/simon_asks/install/templates/questions.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register SimonAsks::Question do
     after_filter :set_user_hook, :only => :create
 
     def set_user_hook
-      question = SimonAsks::Question.where(:email => params[:simon_asks_question][:title]).first
+      question = SimonAsks::Question.where(:title => params[:simon_asks_question][:title]).order('created_at desc').first
       if question
         question.user = current_user
         question.save!
@@ -20,6 +20,25 @@ ActiveAdmin.register SimonAsks::Question do
   filter :title
   filter :content 
   filter :marked
+
+  index do
+    resource_object = controller.resource_class
+    selectable_column
+    column "id", :sortable => :id do |o|
+      o.id
+    end
+    column resource_object.human_attribute_name(:title), :sortable => :title do |obj|
+      link_to obj.title, admin_simon_asks_question_path(obj.id)
+    end  
+    column resource_object.human_attribute_name(:user) do |obj|
+      mail_to obj.user.email if obj.user
+    end                        
+    column I18n.t("active_admin.actions") do |obj|
+     links = link_to I18n.t(:edit), edit_admin_simon_asks_question_path(obj.id)
+     links += '&nbsp;&nbsp;'.html_safe
+     links += link_to I18n.t(:destroy), admin_simon_asks_question_path(obj.id), :confirm => t('are_you_sure', :default => 'Are you sure?'), :method => :delete
+    end
+  end  
 
   form do |f|
     f.actions
@@ -35,7 +54,6 @@ ActiveAdmin.register SimonAsks::Question do
       f.input :tag_list, as: :string
       f.input :content
       f.input :image
-      f.input :user_id, as: :hidden
     end     
     f.actions
   end  
@@ -44,8 +62,9 @@ ActiveAdmin.register SimonAsks::Question do
     attributes_table do
       row :title
       row :tag_list 
-      #row :user do
-      #end
+      row :user do
+        mail_to obj.user.email if obj.user
+      end
       row :content 
       row :image do
         image_tag(obj.image.file.to_s) if obj.image && !obj.image.file.blank?
@@ -53,27 +72,6 @@ ActiveAdmin.register SimonAsks::Question do
       row :created_at
       row :updated_at
     end
-  end
-
-  def index
-    resource_object = controller.resource_class
-    index do 
-      selectable_column
-      column "id", :sortable => :id do |o|
-        o.id
-      end
-      column resource_object.human_attribute_name(:title), :sortable => :title do |obj|
-        link_to obj.title, url_for([:admin, obj])
-      end  
-      column resource_object.human_attribute_name(:user) do |obj|
-        #mail_to obj.user.email if obj.user
-      end                        
-      column I18n.t("active_admin.actions") do |obj|
-       # links = link_to I18n.t(:edit), url_for([:edit, :admin, obj])
-       # links += '&nbsp;&nbsp;'.html_safe
-       # links += link_to I18n.t(:destroy), url_for([:admin, obj]), :confirm => t('are_you_sure', :default => 'Are you sure?'), :method => :delete
-      end
-    end
-  end
+  end 
 
 end
